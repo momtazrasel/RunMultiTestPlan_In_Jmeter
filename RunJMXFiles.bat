822dbb4ff023fee30e639fa69fb18717fa6be7f5
Md. Momtaz Uddin Rashel <38520036+momtazrasel@users.noreply.github.com>
2024-12-26 22:46:38 +0600

    Add files via upload
    Combine Bat File

---
+++ RunJMXFiles.bat
@@ -0,0 +1,3 @@
+start cmd /k jmeter.bat -n -t "C:\Users\RASEL\Downloads\login (1).jmx" -l "C:\Users\RASEL\Desktop\login1.csv"
+start cmd /k jmeter.bat -n -t "C:\Users\RASEL\Downloads\login.jmx" -l "C:\Users\RASEL\Desktop\login2.csv"
+
